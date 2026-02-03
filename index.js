7f8308f167c651d0f718a9ae4e5597b9072e66bc
Keenan <macneill.keenan@gmail.com>
2026-02-03 11:24:49 -0500

    adds strikethrough, inserts new task at top

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const toDoList = document.querySelector('#toDoList')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+  if (input.value !== '') {
+    const newTask = input.value;
+    const newP = document.createElement('p')
 
-  const newTask = input.value;
-  const newP = document.createElement('p')
-
-  newP.textContent = newTask
-  toDoList.appendChild(newP)
+    newP.textContent = newTask
+    const topTask = toDoList.firstElementChild
+    toDoList.insertBefore(newP, topTask)
+  }
 });
 
+toDoList.addEventListener('click', (element) => {
+  if (element.target.nodeName === 'P') { element.target.classList.toggle('strikethrough') }
+})
