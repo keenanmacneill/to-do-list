fe630fe644e56fd3141bc5bb48a42628656635e7
Keenan <macneill.keenan@gmail.com>
2026-02-03 12:16:52 -0500

    adds styling and delete button

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (input.value !== '') {
     const newTask = input.value;
+    const textBox = document.createElement('div')
     const newP = document.createElement('p')
-
-    newP.textContent = newTask
+    const deleteX = document.createElement('p')
     const topTask = toDoList.firstElementChild
-    toDoList.insertBefore(newP, topTask)
+
+    newP.classList.add('task')
+    newP.textContent = newTask
+
+    deleteX.classList.add('deleteX')
+    deleteX.textContent = 'x'
+
+    textBox.classList.add('textBox')
+    textBox.appendChild(newP)
+    textBox.appendChild(deleteX)
+
+    toDoList.insertBefore(textBox, topTask)
   }
 });
 
 toDoList.addEventListener('click', (element) => {
-  if (element.target.nodeName === 'P') { element.target.classList.toggle('strikethrough') }
+  if (element.target.classList.contains('task')) { element.target.classList.toggle('strikethrough') }
 })
+
