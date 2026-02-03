3a2f09dd8ffe77e027b25ea4718cbff5b8e39732
Keenan <macneill.keenan@gmail.com>
2026-02-03 16:00:35 -0500

    saved list to local storage

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const input = document.querySelector('input');
 const form = document.querySelector('form');
-const toDoList = document.querySelector('#toDoList')
+const toDoList = document.querySelector('#toDoList');
+// standardizes saved key in local storage
+const taskKey = 'task';
+// converts JSON in JS array from local storage
+const getTasks = () => {
+  return JSON.parse(localStorage.getItem(taskKey)) || [];
+};
+// converts array into JSON-stringified key-value pairs, saves in local storage
+const saveTasks = (text) => {
+  localStorage.setItem(taskKey, JSON.stringify(text));
+};
+
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -9,6 +20,13 @@ form.addEventListener('submit', (event) => {
     const textBox = document.createElement('div')
     const newP = document.createElement('p')
     const deleteX = document.createElement('p')
+
+    // get current task list from local storage as a JS array
+    const currentTasks = getTasks();
+    // add new task to top of array
+    currentTasks.unshift(newTask);
+    // save task list back in local storage as JSON-stringified key-value pairs
+    saveTasks(currentTasks);
 
     newP.classList.add('task')
     newP.textContent = newTask
@@ -21,6 +39,7 @@ form.addEventListener('submit', (event) => {
     textBox.appendChild(deleteX)
 
     toDoList.insertBefore(textBox, toDoList.firstElementChild)
+
     input.value = ''
   }
 });
@@ -29,6 +48,42 @@ toDoList.addEventListener('click', (element) => {
   if (element.target.classList.contains('task')) { element.target.classList.toggle('strikethrough') }
 })
 
-toDoList.addEventListener('click', (element) => {
-  if (element.target.classList.contains('deleteX')) { element.target.parentElement.remove() }
-})
+toDoList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('deleteX')) {
+    const taskElm =
+      event.target.previousElementSibling.textContent;
+
+    event.target.parentElement.remove();
+
+    // get JS array from local storage with key of taskKey
+    let tasks = JSON.parse(localStorage.getItem(taskKey)) || [];
+    // filter out deleted task from array
+    tasks = tasks.filter(task => task !== taskElm);
+    // save array as new JSON-stringified key-value pairs
+    localStorage.setItem(taskKey, JSON.stringify(tasks));
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  // set value of tasks to JS array from local storage
+  const tasks = getTasks();
+
+  // recreate list by iterating over JS array, inserting task text into task element
+  tasks.forEach(task => {
+    const textBox = document.createElement('div');
+    const newP = document.createElement('p');
+    const deleteX = document.createElement('p');
+
+    newP.classList.add('task');
+    newP.textContent = task;
+
+    deleteX.classList.add('deleteX');
+    deleteX.textContent = 'X';
+
+    textBox.classList.add('textBox');
+    textBox.appendChild(newP);
+    textBox.appendChild(deleteX);
+
+    toDoList.appendChild(textBox);
+  });
+});
